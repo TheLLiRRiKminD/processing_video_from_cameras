b1498bef1f8e1e49c1dadacf280381e7e4e017ce
Kirill <kill2002@mail.ru>
2024-04-24 22:54:54 +0300

    Revision 2.01

--- video/views.py
+++ video/views.py
@@ -1,45 +1,43 @@
 import os
 import time
-import cv2
+import ffmpeg
 from rest_framework.response import Response
 from rest_framework import viewsets
-from .models import Video
-from .serializers import VideoSerializer
+import cv2
+import numpy as np
+from video_frame.models import VideoFrame
 
 
 class VideoViewSet(viewsets.ModelViewSet):
-    queryset = Video.objects.all()
-    serializer_class = VideoSerializer
-
     def create(self, request, **kwargs):
         try:
             rtsp_url = os.getenv('RTSP_CAMERA_URL')
-            cap = cv2.VideoCapture(rtsp_url)
-
-            if not cap.isOpened():
-                return Response({'error': 'Ошибка открытия камеры'}, status=500)
+            process = (
+                ffmpeg
+                .input(rtsp_url, rtsp_transport='tcp',
+                       timeout=0)  # Устанавливаем время ожидания в 0 для бесконечного ожидания
+                .output('pipe:', format='rawvideo', pix_fmt='bgr24')
+                .run_async(pipe_stdout=True)
+            )
 
             frame_count = 0
-            fps = 5
-            interval = 1 / fps
-            start_time = time.time()
 
             while True:
-                ret, frame = cap.read()
-                if not ret:
+                in_bytes = process.stdout.read(1920 * 1080 * 3)  # Размер кадра для Full HD
+                if not in_bytes:
                     break
+
+                frame = np.frombuffer(in_bytes, np.uint8).reshape([1080, 1920, 3])  # Размер кадра для Full HD
 
                 # Применяем медианный фильтр для удаления шума
                 frame = cv2.medianBlur(frame, 5)
 
-                current_time = time.time()
-                elapsed_time = current_time - start_time
+                VideoFrame.objects.create(
+                    name=f'video_frame_{frame_count}.jpg',
+                    frame=frame,
+                )
 
-                if elapsed_time >= interval:
-                    video = Video(name='Video Stream')
-                    video.video_file.save(f'video_frame_{frame_count}.jpg', frame, save=True)
-                    frame_count += 1
-                    start_time = time.time()
+                frame_count += 1
 
             return Response({'message': 'Кадры видео сохранены и обработаны для удаления шума'}, status=200)
         except Exception as e:
