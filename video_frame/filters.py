4b2e7e875e593c4287b4aca22c3369c84b83abaf
llirrik <kill2002@mail.ru>
2024-04-26 12:32:54 +0300

    Revision 1.2

--- video_frame/filters.py
+++ video_frame/filters.py
@@ -2,9 +2,17 @@ import django_filters
 from video_frame.models import VideoFrame
 
 
+class DateCreatedFilter(django_filters.Filter):
+    def filter(self, queryset, value):
+        if value is not None:
+            date_from, date_to = value.split(',')
+            return queryset.filter(created_at__gte=date_from, created_at__lte=date_to)
+        return queryset
+
+
 class VideoFrameFilter(django_filters.FilterSet):
-    date_range = django_filters.DateFromToRangeFilter(field_name='created_at')
+    date_created = DateCreatedFilter()
 
     class Meta:
         model = VideoFrame
-        fields = ['date_range']
+        fields = ['date_created']
--- video_frame/views.py
+++ video_frame/views.py
@@ -1,5 +1,5 @@
+from django_filters.rest_framework.backends import DjangoFilterBackend
 from rest_framework import viewsets
-
 from .filters import VideoFrameFilter
 from .models import VideoFrame
 from .serializers import VideoFrameSerializer
@@ -8,4 +8,6 @@ from .serializers import VideoFrameSerializer
 class VideoFrameViewSet(viewsets.ModelViewSet):
     queryset = VideoFrame.objects.all()
     serializer_class = VideoFrameSerializer
+    filter_backends = [DjangoFilterBackend]
     filter_class = VideoFrameFilter
+
