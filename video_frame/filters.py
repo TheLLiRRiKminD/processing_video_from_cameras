0c536fb900e4103805fa9cfe3f92eb72a2f497dd
Kirill <kill2002@mail.ru>
2024-04-27 14:22:58 +0300

    intermediate commit

--- video_frame/filters.py
+++ video_frame/filters.py
@@ -1,12 +1,21 @@
+from datetime import datetime
 import django_filters
+from rest_framework.response import Response
 from video_frame.models import VideoFrame
+from rest_framework import status
 
 
 class DateCreatedFilter(django_filters.Filter):
     def filter(self, queryset, value):
         if value is not None:
-            date_from, date_to = value.split(',')
-            return queryset.filter(created_at__gte=date_from, created_at__lte=date_to)
+            try:
+                date_from, date_to = value.split(',')
+                date_from = datetime.strptime(date_from, '%Y-%m-%d %H:%M:%S').date()
+                date_to = datetime.strptime(date_to, '%Y-%m-%d %H:%M:%S').date()
+                return queryset.filter(created_at__gte=date_from, created_at__lte=date_to)
+            except ValueError:
+                raise Response({"error": "Invalid date format. Expected YYYY-MM-DD HH:MM:SS"},
+                               status=status.HTTP_400_BAD_REQUEST)
         return queryset
 
 
