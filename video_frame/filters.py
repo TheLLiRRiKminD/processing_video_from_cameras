a7edd4789e452d9cda17f4e2cb33b736b1688965
Kirill <kill2002@mail.ru>
2024-04-27 15:39:29 +0300

    Final version

--- video_frame/filters.py
+++ video_frame/filters.py
@@ -1,10 +1,25 @@
+from datetime import datetime
 import django_filters
+from django.core.exceptions import BadRequest
 from video_frame.models import VideoFrame
 
 
+class DateCreatedFilter(django_filters.Filter):
+    def filter(self, queryset, value):
+        if value is not None:
+            try:
+                date_from, date_to = value.split(',')
+                date_from = datetime.strptime(date_from, '%Y-%m-%d %H:%M:%S').date()
+                date_to = datetime.strptime(date_to, '%Y-%m-%d %H:%M:%S').date()
+                return queryset.filter(created_at__gte=date_from, created_at__lte=date_to)
+            except ValueError:
+                raise BadRequest("Неверный формат даты, попробуйте YYYY-MM-DD HH:MM:SS")
+        return queryset
+
+
 class VideoFrameFilter(django_filters.FilterSet):
-    date_range = django_filters.DateFromToRangeFilter(field_name='created_at')
+    date_created = DateCreatedFilter()
 
     class Meta:
         model = VideoFrame
-        fields = ['date_range']
+        fields = ['date_created']
