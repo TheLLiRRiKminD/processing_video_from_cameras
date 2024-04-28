32248fa45c837536ad534387e0b43a62375fea33
TheLLiRRiKminD <kill2002@mail.ru>
2024-04-28 22:05:10 +0300

    intermediate commit

--- users/management/commands/createsuperusercustom.py
+++ users/management/commands/createsuperusercustom.py
@@ -9,7 +9,14 @@ load_dotenv()
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create()
+        user = User.objects.create(
+            email=f"{os.getenv('SU_EMAIL_ADDRESS')}",
+            first_name='Admin',
+            last_name='Project',
+            is_staff=True,
+            is_superuser=True,
+            is_active=True
+        )
 
         user.set_password(os.getenv('SU_PASSWORD'))
         user.save()
